d520a84d326b9df2a3d6422dbe5035394d18e380
Karina <14150226c@gmail.com>
2026-03-21 15:45:37 +0700

    Update ScheduledProjector.h

--- ScheduledProjector.h
+++ ScheduledProjector.h
@@ -6,54 +6,45 @@
 #include <thread>
 #include <atomic>
 #include <functional>
-#include <mutex>      // Добавлено!
-#include <algorithm>  // Добавлено для remove_if
+#include <mutex>      
+#include <algorithm>  
 
-// Структура для хранения запланированной задачи
 struct ScheduledTask {
-    time_t executeTime;        // Время выполнения
-    std::string action;        // Действие ("turnOn", "turnOff", "play")
-    std::string fileName;      // Имя файла (для play)
-    bool completed;            // Выполнена ли задача
-    std::function<void()> callback; // Функция обратного вызова
+    time_t executeTime;
+    std::string action;
+    std::string fileName;
+    bool completed;
+    std::function<void()> callback;
 };
 
-// Декоратор, добавляющий планировщик задач
 class ScheduledProjector : public ProjectorDecorator {
 private:
     std::vector<ScheduledTask> schedule;
     std::unique_ptr<std::thread> schedulerThread;
     std::atomic<bool> isRunning;
-    mutable std::mutex scheduleMutex;  // Добавлен mutable для const методов
+    mutable std::mutex scheduleMutex;
 
-    // Внутренний метод для проверки и выполнения задач
     void checkSchedule();
 
 public:
-    ScheduledProjector(std::unique_ptr<IProjector> projector);
+    ScheduledProjector(Projector* projector);  // РњРµРЅСЏРµРј РЅР° СЃС‹СЂРѕР№ СѓРєР°Р·Р°С‚РµР»СЊ
     ~ScheduledProjector();
 
-    // Запрещаем копирование
     ScheduledProjector(const ScheduledProjector&) = delete;
     ScheduledProjector& operator=(const ScheduledProjector&) = delete;
 
-    // Методы планирования
     void scheduleTurnOn(time_t execTime);
     void scheduleTurnOff(time_t execTime);
     void schedulePlay(time_t execTime, const std::string& fileName);
 
-    // Отмена задач
     void cancelAllTasks();
     void cancelTask(int index);
 
-    // Просмотр расписания
     void showSchedule() const;
 
-    // Запуск/остановка планировщика
     void startScheduler();
     void stopScheduler();
 
-    // Переопределенные методы
     void turnOn() override;
     void turnOff() override;
     void play(const std::string& fileName) override;
