697818c98fa42573004094451f0f915c88847b25
Karina <14150226c@gmail.com>
2026-03-21 15:46:04 +0700

    Update ProjectorDecorator.h

--- ProjectorDecorator.h
+++ ProjectorDecorator.h
@@ -1,30 +1,21 @@
 #pragma once
 
-#include "IProjector.h"
+#include "Projector.h"
 #include <memory>
 
-// Базовый декоратор для проектора
-class ProjectorDecorator : public IProjector {
+class ProjectorDecorator : public Projector {
 protected:
-    std::unique_ptr<IProjector> wrappedProjector;
+    Projector* wrappedProjector;  // РњРµРЅСЏРµРј РЅР° СЃС‹СЂРѕР№ СѓРєР°Р·Р°С‚РµР»СЊ
 
 public:
-    ProjectorDecorator(std::unique_ptr<IProjector> projector);
+    ProjectorDecorator(Projector* projector);  // РњРµРЅСЏРµРј РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
     virtual ~ProjectorDecorator() = default;
 
-    // Запрещаем копирование
-    ProjectorDecorator(const ProjectorDecorator&) = delete;
-    ProjectorDecorator& operator=(const ProjectorDecorator&) = delete;
-
-    // Делегируем вызовы обернутому объекту
     void turnOn() override;
     void turnOff() override;
     void play(const std::string& fileName) override;
     std::string getStatus() const override;
     std::string getType() const override;
-
-    // Методы из Projector (если нужны)
     void stop() override;
-    int getLampHours() const override;
-    void setLampHours(int hours) override;
+    Projector* getWrappedProjector() const { return wrappedProjector; }
 };
